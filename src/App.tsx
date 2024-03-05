ee64867e89daf1db49dfcf82d43b9ee682711f00
hipo-panda <w6238w2@gmail.com>
2024-03-05 20:55:02 +0900

    ✨ feat: Implement login state management using Context API and add logout functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,45 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import Main from "./views/MainPage";
 import SignUp from "./views/SignUpPage";
 import SignIn from "./views/SignInPage";
 import GamePage from "./views/GamePage";
-
 import SocialCallback from "./views/CallBack";
-
-
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/main" />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/Signup" element={<SignUp />} />
-        <Route path="/Signin" element={<SignIn />} />
-        <Route path="/social/callback/google" element={<SocialCallback provider="google" />}></Route>
-        <Route path="/social/callback/kakao" element={<SocialCallback provider="kakao"/>}></Route>
-        <Route path="/social/callback/naver" element={<SocialCallback provider="naver" />}></Route>
-        <Route path="/social/callback/github" element={<SocialCallback provider="github" />}></Route>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/main" />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route path="/Signin" element={<SignIn />} />
+          <Route
+            path="/social/callback/google"
+            element={<SocialCallback provider="google" />}
+          ></Route>
+          <Route
+            path="/social/callback/kakao"
+            element={<SocialCallback provider="kakao" />}
+          ></Route>
+          <Route
+            path="/social/callback/naver"
+            element={<SocialCallback provider="naver" />}
+          ></Route>
+          <Route
+            path="/social/callback/github"
+            element={<SocialCallback provider="github" />}
+          ></Route>
 
-        <Route path="/Game" element={<GamePage />} />
-       
-      </Routes>
-    </Router>
+          <Route path="/Game" element={<GamePage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
