65a30963358753371fa2b876d68a316ed5e52318
hipo-panda <w6238w2@gmail.com>
2024-03-27 19:26:46 +0900

    ✨ feat : Fix Memorize Page

--- src/views/Memorizepage.tsx
+++ src/views/Memorizepage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import styles from "../css/MemorizePage.module.css";
 import { FaCircleArrowLeft } from "react-icons/fa6";
@@ -19,10 +19,10 @@ const MemorizePage: React.FC = () => {
   const [kanjiStyle, setKanjiStyle] = useState({});
   const { title } = location.state as { title: string };
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [animateX, setAnimateX] = useState<number | string>(0);
+  const [animateX, setAnimateX] = useState<number | string>("0%");
   const y = useMotionValue(0);
   const opacity = useTransform(y, [70, 0], [0, 1]);
-  const scaleY = useTransform(y, [0, 100], [1, 0.2]);
+  const top = useTransform(y, [0, 100], ["0%", "100%"]);
   const [showDetails, setShowDetails] = useState(false);
   const [correctButtonClicked, setCorrectButtonClicked] =
     useState<boolean>(false);
@@ -33,15 +33,21 @@ const MemorizePage: React.FC = () => {
     console.log("title::", title);
   }, [words, title]);
 
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === " ") {
-        setShowDetails(!showDetails);
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.code === "Space") {
+      setShowDetails(true); // 상세 정보 표시 상태를 변경
+      y.set(100); // 커튼을 최대치로 내림
+      if (document.activeElement instanceof HTMLElement) {
+        document.activeElement.blur();
       }
-    };
+    }
+  };
 
+  useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
   }, [showDetails]);
 
   // 이제 알아요 버튼 눌렀을 때 애니메이션
@@ -57,6 +63,8 @@ const MemorizePage: React.FC = () => {
             prevIndex < words.length - 1 ? prevIndex + 1 : prevIndex // 단어를 다음 단어로 변경
         );
         setAnimateX(0);
+        setShowDetails(false); // 상세 정보 표시 상태 초기화
+        y.set(0);
         setCorrectButtonClicked(false);
         setKanjiStyle({ color: "black", scale: 1 });
       }, 500); // 카드가 넘어가는 애니메이션 지속 시간
@@ -75,6 +83,8 @@ const MemorizePage: React.FC = () => {
             prevIndex < words.length - 1 ? prevIndex + 1 : prevIndex // 단어를 다음 단어로 변경
         );
         setAnimateX(0);
+        setShowDetails(false); // 상세 정보 표시 상태 초기화
+        y.set(0);
         setWrongButtonClicked(false);
       }, 500); // 카드가 넘어가는 애니메이션 지속 시간
     }, 200); // 버튼 애니메이션 지속 시간
@@ -84,10 +94,12 @@ const MemorizePage: React.FC = () => {
     <div className={styles.main_container}>
       <div className={styles.info_bar_container}>
         <div className={styles.exit_btn_container}>
-          <button>
-            <FaCircleArrowLeft className={styles.exit_icon} />
-            학습 종료
-          </button>
+          <Link to={`/set/${id}`}>
+            <button>
+              <FaCircleArrowLeft className={styles.exit_icon} />
+              학습 종료
+            </button>
+          </Link>
         </div>
         <div className={styles.title_container}>
           <p>{title}</p>
@@ -121,14 +133,19 @@ const MemorizePage: React.FC = () => {
               <motion.div
                 style={{
                   y,
-                  scaleY,
+                  top,
                   opacity: showDetails ? 1 : opacity,
                 }}
                 drag="y"
+                dragElastic={{ top: 0, bottom: 0.4 }}
                 dragConstraints={{ top: 0, bottom: 0 }}
                 className={styles.bottom_wrap_curtain}
               >
-                {}
+                <p className={styles.curtain_text}>
+                  스페이스를 누르거나 마우스로 내려서
+                  <br />
+                  의미를 확인할 수 있어요!
+                </p>
               </motion.div>
             </motion.div>
           </motion.div>
@@ -140,7 +157,7 @@ const MemorizePage: React.FC = () => {
             transition={{ duration: 0.2 }}
             onClick={() => correctNextWord()}
           >
-            {correctButtonClicked ? "잘했어요!" : "이제 알아요"}
+            {correctButtonClicked ? "잘했어요!" : "알고 있어요!"}
             <BsEmojiSunglassesFill className={styles.happy_emoji} />
           </motion.button>
           <motion.button
